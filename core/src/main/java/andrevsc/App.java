9c5f919aaf18b8aae5e41990dd15ef8dfff7cad8
AndreVsc <95365578+AndreVsc@users.noreply.github.com>
2024-08-20 20:23:49 -0300

    done

---
+++ core/src/main/java/andrevsc/App.java
@@ -0,0 +1,13 @@
+package andrevsc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
